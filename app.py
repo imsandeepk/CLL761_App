2b4d2d98679a6ad384878a29fd5bddf2b7468943
Sandeep <65333883+imsandeepk@users.noreply.github.com>
2025-04-14 20:35:18 +0530

    wokring

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 # Backend API URL (replace with your actual endpoint)
-API_URL = "http://0.0.0.0:5000/get_results"
+API_URL = "https://sandeep.abhi.rip//get_results"
 
 # Index options from the image
 index_options  = [
